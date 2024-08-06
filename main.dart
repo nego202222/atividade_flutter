f57c71f4b8968cd42f9c13c5c782699002d9fa44
Davi77 <149195073+nego202222@users.noreply.github.com>
2024-08-06 17:10:34 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:ola_mundo2/componentes/body.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu Aplicativo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('steve'),
+        ),
+        body: const Body(),
+      ),
+    );
+  }
+}
